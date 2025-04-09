1a0985f98efc7564dd8075ae89d0e27fd6c579ea
Marcin Wlodarski <mwlodarski@icloud.com>
2025-04-09 15:45:37 +0200

    server
    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,69 +8,76 @@ import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import AstroPWA from "@vite-pwa/astro";
 import icon from "astro-icon";
+import sveltiaCms from "astro-sveltia-cms";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astros.zank.studio",
-	vite: {
-		define: {
-			__DATE__: `'${new Date().toISOString()}'`,
-		},
-	},
-	integrations: [
-		tailwind(),
-		sitemap(),
-		astroI18next(),
-		alpinejs(),
-		AstroPWA({
-			mode: "production",
-			base: "/",
-			scope: "/",
-			includeAssets: ["favicon.svg"],
-			registerType: "autoUpdate",
-			manifest: {
-				name: "Astros - Starter Template for Astro with Tailwind CSS",
-				short_name: "Astros",
-				theme_color: "#ffffff",
-				icons: [
-					{
-						src: "pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-			},
-			workbox: {
-				navigateFallback: "/404",
-				globPatterns: ["*.js"],
-			},
-			devOptions: {
-				enabled: false,
-				navigateFallbackAllowlist: [/^\/404$/],
-				suppressWarnings: true,
-			},
-		}),
-		icon(),
-	],
-	markdown: {
-		rehypePlugins: [
-			rehypeSlug,
-			// This adds links to headings
-			[rehypeAutolinkHeadings, autolinkConfig],
-		],
-	},
-	experimental: {
-		contentCollectionCache: true,
-	},
+  site: "https://makiorganizacja.nl",
+  vite: {
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
+  output: "server", //
+  integrations: [
+    sveltiaCms(),
+    decapCmsOauth({
+      decapCMSSrcUrl: "https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js",
+    }),
+    tailwind(),
+    sitemap(),
+    astroI18next(),
+    alpinejs(),
+
+    AstroPWA({
+      mode: "production",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Astros - Starter Template for Astro with Tailwind CSS",
+        short_name: "Astros",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["*.js"],
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallbackAllowlist: [/^\/404$/],
+        suppressWarnings: true,
+      },
+    }),
+    icon(),
+  ],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      // This adds links to headings
+      [rehypeAutolinkHeadings, autolinkConfig],
+    ],
+  },
+  experimental: {
+    contentCollectionCache: true,
+  },
 });
